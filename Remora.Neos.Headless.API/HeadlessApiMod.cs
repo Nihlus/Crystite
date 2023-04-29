8991546153e15d635c40c41eaf1695318fdc705c
Jarl Gullberg <jarl.gullberg@gmail.com>
2023-04-29 23:33:00 +0200

    Configure the http server.

--- Remora.Neos.Headless.API/HeadlessApiMod.cs
+++ Remora.Neos.Headless.API/HeadlessApiMod.cs
@@ -4,7 +4,12 @@
 //  SPDX-License-Identifier: AGPL-3.0-or-later
 //
 
+using System;
 using System.Reflection;
+using FrooxEngine;
+using Grapevine;
+using Microsoft.Extensions.DependencyInjection;
+using NeosHeadless;
 using NeosModLoader;
 
 namespace Remora.Neos.Headless.API;
@@ -14,6 +19,7 @@ namespace Remora.Neos.Headless.API;
 /// </summary>
 public class HeadlessApiMod : NeosMod
 {
+    [AutoRegisterConfigKey]
     private readonly ModConfigurationKey<string> _listenAddressKey = new
     (
         "listen_address",
@@ -21,12 +27,15 @@ public class HeadlessApiMod : NeosMod
         () => "127.0.0.1"
     );
 
+    [AutoRegisterConfigKey]
     private readonly ModConfigurationKey<ushort> _listenPortKey = new
     (
         "listen_port",
         "The port to listen for REST requests on.",
         () => 12594
     );
+
+    private IRestServer _server = null!;
 
     /// <inheritdoc />
     public override string Name => "Headless REST API";
@@ -44,15 +53,39 @@ public class HeadlessApiMod : NeosMod
     public override void OnEngineInit()
     {
         base.OnEngineInit();
-    }
 
-    /// <inheritdoc />
-    public override void DefineConfiguration(ModConfigurationDefinitionBuilder builder)
-    {
-        base.DefineConfiguration(builder);
-        builder
-            .Version(this.Version)
-            .Key(_listenAddressKey)
-            .Key(_listenPortKey);
+        var modConfig = GetConfiguration() ?? throw new InvalidOperationException();
+
+        if (!modConfig.TryGetValue(_listenAddressKey, out var listenAddress))
+        {
+            Error("No listen address defined.");
+            return;
+        }
+
+        if (!modConfig.TryGetValue(_listenPortKey, out var listenPort))
+        {
+            Error("No listen port defined.");
+            return;
+        }
+
+        var configField = Assembly.GetAssembly(typeof(WorldHandler))
+            .GetType("Program")
+            .GetField("config", BindingFlags.NonPublic | BindingFlags.Static)
+            ?? throw new InvalidOperationException();
+
+        var serverBuilder = RestServerBuilder.UseDefaults();
+
+        serverBuilder.ConfigureServer = s =>
+        {
+            s.Prefixes.Add($"http://{listenAddress}:{listenPort}");
+        };
+
+        serverBuilder.ConfigureServices = s => s
+            .AddSingleton(Engine.Current)
+            .AddSingleton(Engine.Current.WorldManager)
+            .AddSingleton((NeosHeadlessConfig)configField.GetValue(null));
+
+        _server = serverBuilder.Build();
+        _server.Start();
     }
 }
