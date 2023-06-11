7d141533891c1b01d71658b4a2012ab38cd74a97
Jarl Gullberg <jarl.gullberg@gmail.com>
2023-06-11 15:15:03 +0200

    Use the type translator in two places of the save routines.
    Fixes #2.

---
+++ Remora.Neos.Headless/Patches/SaveControl/UseForwardedTypeName.cs
@@ -0,0 +1,49 @@
+//
+//  SPDX-FileName: UseForwardedTypeName.cs
+//  SPDX-FileCopyrightText: Copyright (c) Jarl Gullberg
+//  SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+using System.Reflection.Emit;
+using HarmonyLib;
+using JetBrains.Annotations;
+using Remora.Neos.Headless.Patches.WorkerManager;
+
+namespace Remora.Neos.Headless.Patches.SaveControl;
+
+/// <summary>
+/// Replaces use of FullName with a serializable type name.
+/// </summary>
+[HarmonyPatch(typeof(FrooxEngine.SaveControl), nameof(FrooxEngine.SaveControl.StoreTypeVersions))]
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public class UseForwardedTypeName
+{
+    /// <summary>
+    /// Transpiles the original IL into something new.
+    /// </summary>
+    /// <param name="instructions">The instructions of the method.</param>
+    /// <returns>The patched code.</returns>
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var fullNameGetter = typeof(Type).GetProperty(nameof(Type.FullName))?.GetMethod
+                             ?? throw new InvalidOperationException();
+
+        var getSerializableNameMethod = typeof(ForwardedTypeSerialization)
+            .GetMethod(nameof(ForwardedTypeSerialization.GetSerializableTypeName))
+                ?? throw new InvalidOperationException();
+
+        foreach (var instruction in instructions)
+        {
+            if (instruction.Calls(fullNameGetter))
+            {
+                yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+                yield return new CodeInstruction(OpCodes.Call, getSerializableNameMethod);
+            }
+            else
+            {
+                yield return instruction;
+            }
+        }
+    }
+}
---
+++ Remora.Neos.Headless/Patches/Worker/UseForwardedTypeName.cs
@@ -0,0 +1,35 @@
+//
+//  SPDX-FileName: UseForwardedTypeName.cs
+//  SPDX-FileCopyrightText: Copyright (c) Jarl Gullberg
+//  SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+using HarmonyLib;
+using JetBrains.Annotations;
+using Remora.Neos.Headless.Patches.WorkerManager;
+
+namespace Remora.Neos.Headless.Patches.Worker;
+
+#pragma warning disable SA1313
+
+/// <summary>
+/// Replaces the type name of the worker with a serializable type name.
+/// </summary>
+[HarmonyPatch(typeof(FrooxEngine.Worker), nameof(FrooxEngine.Worker.WorkerTypeName), MethodType.Getter)]
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public static class UseForwardedTypeName
+{
+    /// <summary>
+    /// Replaces the type name of the worker with a serializable type name.
+    /// </summary>
+    /// <param name="__instance">The instance.</param>
+    /// <param name="__result">The new result.</param>
+    /// <returns>Always false.</returns>
+    // ReSharper disable InconsistentNaming
+    // ReSharper disable RedundantAssignment
+    public static bool Prefix(FrooxEngine.Worker __instance, ref string __result)
+    {
+        __result = __instance.WorkerType.GetSerializableTypeName();
+        return false;
+    }
+}
