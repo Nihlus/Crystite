9201a95b34f41c1467315f8110aebffd00667cb0
Jarl Gullberg <jarl.gullberg@gmail.com>
2023-06-11 15:14:05 +0200

    Add a unit test project.

---
+++ Tests/Remora.Neos.Headless.Tests/TypeNameSerializationTests.cs
@@ -0,0 +1,35 @@
+//
+//  SPDX-FileName: TypeNameSerializationTests.cs
+//  SPDX-FileCopyrightText: Copyright (c) Jarl Gullberg
+//  SPDX-License-Identifier: AGPL-3.0-or-later
+//
+
+using System;
+using FrooxEngine;
+using Remora.Neos.Headless.Patches.WorkerManager;
+using Xunit;
+
+namespace Remora.Neos.Headless.Tests;
+
+/// <summary>
+/// Tests the <see cref="ForwardedTypeSerialization"/> patch's underlying functionality.
+/// </summary>
+public class TypeNameSerializationTests
+{
+    /// <summary>
+    /// Tests whether the given type's name is correctly serialized.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <param name="qualifyAssembly">Whether to fully quality the assembly.</param>
+    /// <param name="expected">The expected output.</param>
+    [Theory]
+    [InlineData(typeof(string), false, "System.String")]
+    [InlineData(typeof(string), true, "System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
+    [InlineData(typeof(ValueTextFormatDriver<float>), false, "FrooxEngine.ValueTextFormatDriver`1[[System.Single, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]")]
+    [InlineData(typeof(DynamicField<string>), false, "FrooxEngine.DynamicField`1[[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]")]
+    public void SerializesNameCorrectly(Type type, bool qualifyAssembly, string expected)
+    {
+        var actual = type.GetSerializableTypeName(qualifyAssembly);
+        Assert.Equal(expected, actual);
+    }
+}
