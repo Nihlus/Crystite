14b8a3bfb0f5a89fda2c7dc67fa19bd04e078603
djsime1 <dj@dj.je>
2024-05-24 14:30:11 -0500

    Remove dotnet 7 and steamcmd dependencies from Docker image, add modloader install to entrypoint.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,27 +3,57 @@ echo "Required Steam credentials have not been set! Are you sure they are in the
 exit 1
 fi
 
-echo "=== INSATLLING/UPDATING RESONITE ==="
-if [ -v HEADLESS_KEY ]; then
-/usr/games/steamcmd \
-    +force_install_dir /var/lib/crystite/Resonite \
-    +login $STEAM_CRED $STEAM_PASS \
-    +app_license_request 2519830 \
-    +app_update 2519830 -beta headless -betapassword $HEADLESS_KEY validate \
-    +quit
-else
-/usr/games/steamcmd \
-    +force_install_dir /var/lib/crystite/Resonite \
-    +login $STEAM_CRED $STEAM_PASS \
-    +app_license_request 2519830 \
-    +app_update 2519830 validate \
-    +quit
-fi
-
 echo "=== INSTALLING HEADLESS CONFIG FILE ==="
 echo "Selected file: Config/${CONFIG_FILE:=Config.json}"
 RESONITE_CONFIG=$(grep -v " null," "/Config/$CONFIG_FILE")
 echo "{ \"Resonite\": $RESONITE_CONFIG }" > /etc/crystite/conf.d/resonite.json
+
+echo "=== INSATLLING/UPDATING RESONITE ==="
+cat >/etc/crystite/conf.d/steamcreds.json <<EOF
+{
+    "Headless": {
+        "resonitePath": "/var/lib/crystite/Resonite",
+        "manageResoniteInstallation": true,
+        "steamCredential": "$STEAM_CRED",
+        "steamPassword": "$STEAM_PASS"
+    },
+}
+EOF
+/usr/lib/crystite/crystite --install-only --ignore-version-mismatch
+
+if [ ${MODLOADER:=None} != "None" ]; then
+echo "=== INSTALLING MODLOADER ==="
+case $MODLOADER in
+  RML)
+    echo "Selected modloader: ResoniteModLoader"
+    mkdir \
+        /var/lib/crystite/Resonite/Libraries \
+        /var/lib/crystite/Resonite/rml_mods \
+        /var/lib/crystite/Resonite/rml_libs \
+        /var/lib/crystite/Resonite/rml_config
+    wget -O /var/lib/crystite/Resonite/Libraries/ResoniteModLoader.dll "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/ResoniteModLoader.dll"
+    wget -O /var/lib/crystite/Resonite/rml_libs/0Harmony.dll "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/0Harmony.dll"
+    cat >/etc/crystite/conf.d/modloader.json <<EOF
+{
+    "Resonite": {
+        "pluginAssemblies": "/var/lib/crystite/Resonite/Libraries/ResoniteModLoader.dll",
+    },
+}
+EOF
+    ;;
+  MonkeyLoader)
+    echo "Selected modloader: MonkeyLoader"
+    ;;
+  MonkeyLoaderRML)
+    echo "Selected modloader: MonkeyLoader + ResoniteModLoader"
+    ;;
+  *)
+    echo "Unrecognized modloader \"$MODLOADER\", skipping installation."
+    ;;
+esac
+else
+echo "No modloader selected, skipping installation."
+fi
 
 echo "=== STARTING CRYSTITE ==="
 /usr/lib/crystite/crystite
